fdd552f17eb4fae71ba31a008990fbab97815394
Delmar Zhao <delmar.zhao@fortrantraffic.com>
2019-07-08 11:52:02 -0400

    fix syntax

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -86,7 +86,7 @@ class LiveMapController extends StatefulMapController{
         point: point,
         width: 80.0,
         height: 80.0,
-        builder: _liveMarkerWidgetBuilder;
+        builder: _liveMarkerWidgetBuilder);
     _liveMarker = liveMarker;
     await addMarker(marker: _liveMarker, name: "livemarker");
   }
